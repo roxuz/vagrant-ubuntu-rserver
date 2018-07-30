8dc4cbda752669cb535dc4e4b20564f1efc9270b
Eike Steinig <jc225327@ad.jcu.edu.au>
2018-07-31 08:17:35 +1000

    fix bug in config.sh

--- config.sh
+++ config.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
-gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
-gpg -a --export E084DAB9 | sudo apt-key add -
-
-sudo apt-get update
-sudo apt-get install r-base r-base-dev
-sudo apt-get install gdebi-core
-
-wget https://download2.rstudio.org/rstudio-server-1.1.456-amd64.deb
-sudo gdebi -n rstudio-server-1.1.456-amd64.deb
-sudo rstudio-server verify-installation
+#!/bin/bashsudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.listgpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9gpg -a --export E084DAB9 | sudo apt-key add -sudo apt-get updatesudo apt-get install r-base r-base-devsudo apt-get install gdebi-corewget https://download2.rstudio.org/rstudio-server-1.1.456-amd64.debsudo gdebi -n rstudio-server-1.1.456-amd64.deb
